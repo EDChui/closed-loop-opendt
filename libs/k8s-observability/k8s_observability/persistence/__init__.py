e431e556c2b08293ce1f62dd1c5c148ce3f63dba
EDChui <edchuihk@gmail.com>
2026-04-13 20:49:22 +0200

    add: k8s-observer capture node cpu and memory utilization

--- libs/k8s-observability/k8s_observability/persistence/__init__.py
+++ libs/k8s-observability/k8s_observability/persistence/__init__.py
@@ -1,15 +1,17 @@
 from k8s_observability.persistence.sqlachemy.base import Base
 from k8s_observability.persistence.sqlachemy.session import build_engine, build_session_factory, test_connection
-from k8s_observability.persistence.sqlachemy.tables import K8sTaskRecordRow, K8sResourceUsageSnapshotRow, NodePowerReadingRow
+from k8s_observability.persistence.sqlachemy.tables import K8sTaskRecordRow, K8sResourceUsageSnapshotRow, NodePowerReadingRow, NodeUtilizationSnapshotRow
 from k8s_observability.persistence.sqlachemy.mappers import (
     task_record_to_row,
     row_to_task_record,
     resource_usage_snapshot_to_row,
     row_to_resource_usage_snapshot,
     node_power_reading_to_row,
-    row_to_node_power_reading
+    row_to_node_power_reading,
+    node_utilization_snapshot_to_row,
+    row_to_node_utilization_snapshot
 )
-from k8s_observability.persistence.sqlachemy.repositories import K8sTaskRecordRepository, K8sResourceUsageSnapshotRepository, NodePowerReadingRepository
+from k8s_observability.persistence.sqlachemy.repositories import K8sTaskRecordRepository, K8sWorkloadResourceUsageSnapshotRepository, NodePowerReadingRepository, NodeResourceUsageSnapshotRepository
 
 __all__ = [
     "Base",
@@ -19,13 +21,17 @@ __all__ = [
     "K8sTaskRecordRow",
     "K8sResourceUsageSnapshotRow",
     "NodePowerReadingRow",
+    "NodeUtilizationSnapshotRow",
     "task_record_to_row",
     "row_to_task_record",
     "resource_usage_snapshot_to_row",
     "row_to_resource_usage_snapshot",
     "node_power_reading_to_row",
     "row_to_node_power_reading",
+    "node_utilization_snapshot_to_row",
+    "row_to_node_utilization_snapshot",
     "K8sTaskRecordRepository",
-    "K8sResourceUsageSnapshotRepository",
-    "NodePowerReadingRepository"
+    "K8sWorkloadResourceUsageSnapshotRepository",
+    "NodePowerReadingRepository",
+    "NodeResourceUsageSnapshotRepository"
 ]
